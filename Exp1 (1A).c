c0b4601e4d830bce8ac2837c8854d33bed08fbc3
abdurrahimpavaskar1425-source <abdurrahimpavaskar1425@eng.rizvi.edu.in>
2025-10-21 21:54:27 +0530

    Add files via upload

---
+++ Exp1 (1A).c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name : Abdur Rahim Mohammed Asif Pavaskar \n");
+    printf("Age : 18 years \n");
+    printf("Class : FE/Mechanical \n");
+    printf("Division : F\n");
+    printf("UIN : 251M043\n");
+return 0;
+}
