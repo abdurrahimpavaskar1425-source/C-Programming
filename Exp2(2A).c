cf9ef2e15700064df9208b8a20e93b12d5ae7dc3
abdurrahimpavaskar1425-source <abdurrahimpavaskar1425@eng.rizvi.edu.in>
2025-10-21 22:51:32 +0530

    Add files via upload

---
+++ Exp2(2A).c
@@ -0,0 +1,26 @@
+/*
+Name: Abdur Rahim Mohammed Asif Pavaskar
+UIN: 251M043
+Class: FE/Mechanical
+Div: F
+*/
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number :");
+    scanf("%d", &num);
+
+    if(num%2==0)
+    {
+        printf("num is even");
+    }
+    else
+    {
+        printf("num is odd");
+    }
+
+    return 0;
+}
